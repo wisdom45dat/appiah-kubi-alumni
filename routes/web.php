4dd5d3503805b2cc2fc1c8dd8abfdc89ef3872d6
wisdom45dat <elijahkyeremeh6@gmail.com>
2025-11-17 15:12:01 +0000

    Force HTTPS in production environment

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\URL;
+
+// Force HTTPS in production
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
+
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\GalleryController;
